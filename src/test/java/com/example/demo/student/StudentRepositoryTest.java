7c202647c0553961089012300918105dbc4b8051
AndreyJavaEdu <scream123456@yandex.ru>
2024-02-23 00:18:04 +0300

    Добавили еще один тест метод в котором протестировали: существует ли такой студент в таблице.

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -2,16 +2,17 @@ package com.example.demo.student;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@DataJpaTest
 class StudentRepositoryTest {
     @Autowired
     private StudentRepository underTest;
 
     @Test
-    void itShouldCheckIfStudentExistsEmail() {
+    void itShouldCheckIfStudentEmailExists() {
         //given
         String email = "jenia@gmaIil.com";
         Student student = new Student(
@@ -25,5 +26,17 @@ class StudentRepositoryTest {
 
         //then
         assertThat(expected).isTrue();
+    }
+
+    @Test
+    void itShouldCheckIfStudentEmailDoesNotExists() {
+        //given
+        String email = "jenia@gmaIil.com";
+
+        //when
+        Boolean expected = underTest.selectExistsEmail(email);
+
+        //then
+        assertThat(expected).isFalse();
     }
 }
