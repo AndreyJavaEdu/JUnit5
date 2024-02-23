d7c92f443059f8ba4b950925eb2b1a4d7fdbc3bf
AndreyJavaEdu <scream123456@yandex.ru>
2024-02-23 04:09:21 +0300

    Провели тест варификация вызова метода findAll() внутри метода getAllStudents().

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,25 +1,38 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 class StudentServiceTest {
     @Mock
-    private StudentRepository repository;
+    private StudentRepository studentRepository;
     private AutoCloseable autoCloseable;
     private StudentService underTest;
 
     @BeforeEach
     void setUp() {
-        autoCloseable = MockitoAnnotations.openMocks(StudentServiceTest.class);
-        underTest = new StudentService(repository);
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new StudentService(studentRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
     }
 
     @Test
     void canGetAllStudents() {
+        //when
+        underTest.getAllStudents();
+        //then
+        verify(studentRepository, times(1)).findAll();
 
     }
 
